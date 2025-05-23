173a170ff39524ef24e73d41be80677655883757
Dani Dickstein <daniel.dickstein@gmail.com>
2025-05-23 00:39:36 -0400

    Stay up to date

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@
 #
 opam update
 opam switch create . --empty -y --repos default,dune-universe=https://github.com/dune-universe/opam-overlays.git
+opam switch set-invariant ocaml-base-compiler
 echo "Now running \`eval \$(opam env)\`"
 eval $(opam env)
 opam install ocaml-lsp-server ocamlformat opam-monorepo mdx utop -y
